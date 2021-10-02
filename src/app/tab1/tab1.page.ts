19e3954ca8f0a1649c30fad3da57ffd26346dafe
dinesh <manit.dinesh@gmail.com>
2021-10-02 11:41:43 +0530

    initial commit

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import * as firebaseui from 'firebaseui';
+
 
 @Component({
   selector: 'app-tab1',
@@ -7,6 +9,9 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
+
   constructor() {}
+
+
 
 }
